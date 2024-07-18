2f94ad85095e45f235e4cff10d300f8fff890e5c
Firozrafi <firozrafi123@gmail.com>
2024-07-19 00:47:05 +0530

    Create JVM.java

---
+++ JVM.java
@@ -0,0 +1,45 @@
+public class JVM {
+
+
+
+	public static void main(String[] args) {
+
+
+
+		System.out.println("JVM stands for Java virtual machine");
+
+		System.out.println("JVM allows java program to run on any device");
+
+		System.out.println("it executes java bytecode,which is a compiled version of java source code");
+
+		System.out.println("it as many components");
+
+		System.out.println("loads class files into memory ");
+
+		System.out.println("executes bytecode instructions");
+
+		System.out.println("JVM supprots another languages like scala,kotlin");
+
+		System.out.println("JVM providing platform independent");
+
+		System.out.println("converts bytecode into native machine code to improve performance");
+
+		System.out.println("JVM uses a heap structure for dynamic memory allocation ");
+
+		System.out.println("JVM design ensures security,portability,and performance");
+
+		System.out.println("Stack: Stores local variables, operand stack, and method data per thread.");
+
+		System.out.println("Program Counter Register: Holds the address of the current JVM instruction for each thread.");
+
+		System.out.println("Native Method Stack: Supports native methods written in languages like C/C++.");
+
+		System.out.println("Java Native Interface (JNI): Allows Java code to interact with native applications and libraries.");
+
+		System.out.println("Class Loader: Dynamically loads, links, and initializes classes at runtime.");
+
+	}
+
+
+
+}
