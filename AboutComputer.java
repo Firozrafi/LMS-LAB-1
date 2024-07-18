ac7b745e46cf8abf0e5c819673662397b0330d5b
Firozrafi <firozrafi123@gmail.com>
2024-07-19 00:44:08 +0530

    Create AboutComputer.java

---
+++ AboutComputer.java
@@ -0,0 +1,20 @@
+public class AboutComputer {
+
+	public static void main(String[] args) {
+
+		System.out.println("A computer is an electronic device that processes data based on instructions.");
+		System.out.println("Key components include CPU, RAM, storage, and I/O devices.");
+		System.out.println("Types include desktops, laptops, tablets, and servers.");
+		System.out.println("ENIAC was the first electronic digital computer, developed in the 1940s.");
+		System.out.println("Software includes operating systems and applications.");
+		System.out.println("Computers connect to networks, such as the internet. ");
+		System.out.println("Processing power is measured in GHz and cores.");
+        System.out.println("Storage capacity is measured in GB and TB.");
+		System.out.println("Peripherals like printers and scanners enhance functionality.");
+		System.out.println("Advances in AI, quantum computing, and miniaturization drive innovation.");
+		System.out.println("This all about computer system");
+		
+		
+	}
+
+}
