9b057bb1384315b66be8f5fbbf96c2f753d6241d
Firozrafi <firozrafi123@gmail.com>
2024-07-19 00:38:02 +0530

    Create Introductions.java

---
+++ Introductions.java
@@ -0,0 +1,33 @@
+public class Introduction {
+
+
+
+	public static void main(String[] args) {
+
+
+
+		System.out.println("my name is LALSAHEBGARI FIROZ RAFI");
+
+		System.out.println("I am a student");
+
+		System.out.println("I am from NANDYAL");
+
+		System.out.println("I have completed my degree");
+
+		System.out.println("I am pursing MCA in AVNT");
+
+		System.out.println("my hobbies are CODING,listening to music");
+
+		System.out.println("Iam self motivated");
+
+		System.out.println("I am a quick learner");
+
+		System.out.println("my father name is L.HAROON RASHID");
+
+		System.out.println("my mother name is L.HUSSIAN BEE");
+
+	}
+
+
+
+}
